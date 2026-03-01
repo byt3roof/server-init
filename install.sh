f437c2be29a90fde7c5b31fb3af70ca70f7ea7d2
Pepe <josepp2508@gmail.com>
2026-03-01 01:37:28 -0600

    [FEAT] docker installation func

--- install.sh
+++ install.sh
@@ -51,3 +51,28 @@ install_bunjs() {
   fi
 }
 
+install_docker() {
+  if exists docker: then
+    echo "docker is already installed, skipping..."
+  else
+    echo "--- installing docker engine & compose ---"
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    chmod a+r /etc/apt/keyrings/docker.gpg
+
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    apt update
+    apt install -y docker-ce docker-ce-cli conainerd.io docker-buildx-plugin docker-compose-plugin
+
+    systemctl enable docker --now
+
+    if [ -n "$SUDO_USER" ]; then
+      usermod -aG docker "$SUDO_USER"
+      echo "user $SUDO_USER added to docker group"
+    fi
+  fi
+}
+
