d6ce38ccff610fd2684bbaa15d5fb016b2587076
pepe <josepp2508@gmail.com>
2026-03-02 23:32:51 -0600

    [FEAT] debian support added

--- install.sh
+++ install.sh
@@ -9,6 +9,10 @@ set -e
 
 GO_VERSION="1.26.0"
 
+# OS detection (ubuntu or debian)
+OS_ID=$(grep -oP '(?<=^ID=).*' /etc/os-release | tr -d '"')
+echo "detected OS: $OS_ID"
+
 # Architechture detection
 ARCH=$(uname -m)
 case $ARCH in
@@ -26,7 +30,7 @@ exists() {
 setup_environment() {
   echo "--- updating sys packages ---"
   apt update -y && apt upgrade -y
-  apt-get install -y curl wget git build-essential unzip
+  apt-get install -y curl wget git build-essential unzip lsb-release
 }
 
 install_go() {
@@ -57,11 +61,11 @@ install_docker() {
   else
     echo "--- installing docker engine & compose ---"
     install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    curl -fsSL "https://download.docker.com/linux/${OS_ID}/gpg" | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     chmod a+r /etc/apt/keyrings/docker.gpg
 
     echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/${OS_ID} \
       $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
     apt update
